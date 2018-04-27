ba2acad1151246fe78b3cf070638fba08d0b20ca
suxy <1058952377@qq.com>
2018-04-27 18:45:20 +0800

    download tieba pic

--- CrawlerTieBaList.py
+++ CrawlerTieBaList.py
@@ -4,21 +4,42 @@
 import urllib.request
 import re
 
-import DownloadPagePic
+from DownloadPagePic import *
 
 def getUrlList(url):
-    dp = DownloadPagePic("12","34");
+    dp = DownloadPagePic();
     html = dp.getHtmlContent(url);
+    # print(html);
+    urls = getUrls(html);
+
+
+    print(urls);
+
+    urlPre = "http://tieba.baidu.com";
+    pathPre = "D://crawler";
+
+    for url in urls:
+        dp.setPath(pathPre + url);
+        dp.setUrl(urlPre + url);
+        dp.download();
+
+
     # html = html.decode
 
 
 def getUrls(html):
 
-    urlReg = re.compile('href="(.*?)"');
 
-    # html = html.decode("utf-8");
-    urls = re.findall(urlReg, html);
-    print(urls);
+    html = html.decode("utf-8");
+    # print(html);
+
+    htmlContentReg = re.compile('<ul id="thread_list" class="threadlist_bright j_threadlist_bright">(.*)<div class="thread_list_bottom clearfix">',re.S);
+    htmlContent = re.findall(htmlContentReg, html);
+    print(htmlContent);
+
+    urlReg = re.compile('<a rel=\"noreferrer\"  href="([^:]*?)" title=');
+    urls = re.findall(urlReg, htmlContent[0]);
+    return urls;
 
 if __name__ == '__main__':
-    getUrls("<a rel=\"noreferrer\" href=\"/p/5666211348\" title=\"记录一下\" target=\"_blank\" class=\"j_th_tit\"  href=\"/p/5666211349\" clicked=\"true\">记录一下</a>");
+    getUrlList("http://tieba.baidu.com/f?kw=%E6%91%84%E5%BD%B1");
--- DownloadPagePic.py
+++ DownloadPagePic.py
@@ -3,43 +3,59 @@
 # 实现一个简单的爬虫，爬取百度贴吧图片
 import urllib.request
 import re
-
+import os, sys
 
 class DownloadPagePic :
-
-    def __init__(self, path,url):
-        self.path = path;
-        self.url = url
+    path = '';
+    url = "";
 
     # 根据url获取网页html内容
-    def getHtmlContent(url):
+    def getHtmlContent(self,url):
         page = urllib.request.urlopen(url);
         return page.read()
 
 
     # 从html中解析出所有jpg图片的url
     # 百度贴吧html中jpg图片的url格式为：<img ... src="XXX.jpg" width=...>
-    def getJPGs(html):
+    def getJPGs(self,html):
         # 解析jpg图片url的正则
-        jpgReg = re.compile(r'<img.+?src="(.+?\.jpg)" width')  # 注：这里最后加一个'width'是为了提高匹配精确度
+        jpgReg = re.compile(r'<img.+?src="(.+?\.jpg)".+?>')  # 注：这里最后加一个'width'是为了提高匹配精确度
         # 解析出jpg的url列表
-        html = html.decode('utf-8')  # python3
-        jpgs = re.findall(jpgReg, html)
+        try:
+            html = html.decode('utf-8')  # python3
+            jpgs = re.findall(jpgReg, html)
+        except UnicodeDecodeError:
+            print("error:url:"+self.url);
+            jpgs = [];
 
         return jpgs
 
+    def mkdirs(self,path):
+
+        if os.path.exists(path):
+            pass
+        else:
+            os.makedirs(path);
+
 
     # 用图片url下载图片并保存成制定文件名
-    def downloadJPG(imgUrl, fileName):
-        urllib.request.urlretrieve(imgUrl, fileName)
+    def downloadJPG(self,imgUrl, fileName):
+
+        try:
+            urllib.request.urlretrieve(imgUrl, fileName)
+        except urllib.error.HTTPError:
+            print(imgUrl + ";" + fileName);
+        except UnicodeEncodeError:
+            print(imgUrl + ";" + fileName);
 
 
     # 批量下载图片，默认保存到当前目录下
     def batchDownloadJPGs(self,imgUrls, path='D://crawler//'):
         # 用于给图片命名
         count = 1
+        self.mkdirs(path);
         for url in imgUrls:
-            self.downloadJPG(url, ''.join([path, '{0}.jpg'.format(count)]))
+            self.downloadJPG(url, ''.join([path, '\\{0}.jpg'.format(count)]))
             count = count + 1
 
 
@@ -47,7 +63,13 @@ class DownloadPagePic :
     def download(self):
         html = self.getHtmlContent(self.url);
         jpgs = self.getJPGs(html);
-        self.batchDownloadJPGs(jpgs)
+        self.batchDownloadJPGs(jpgs,self.path)
+
+    def setPath(self,path):
+        self.path = path;
+
+    def setUrl(self,url):
+        self.url = url;
 
     def testMethod(self):
         print("aa");
@@ -59,5 +81,6 @@ class DownloadPagePic :
     #     download(url)
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    dp = DownloadPagePic();
+    os.makedirs('D:\\crawler\\p\\222')
